c9190657db7d2a60397ab9dc9801ec1d0bbae26e
villar <ribeiros@outlook.com>
2023-01-09 18:51:32 -0300

    bean config

---
+++ src/main/java/com/spring/security/controllers/Greetings.java
@@ -0,0 +1,23 @@
+package com.spring.security.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class Greetings {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from our API");
+    }
+
+    @GetMapping("/say-good-bye")
+    public ResponseEntity<String> sayGoodBye() {
+        return ResponseEntity.ok("Say good bye and see you later");
+    }
+
+}
